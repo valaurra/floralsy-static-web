348a005949001b916fbbe659ced4c5cac90544b8
Laura Valentikova <valentikovaalaura@gmail.com>
2024-01-11 19:45:28 +0100

    feat(*); final implementation

---
+++ script.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+
+window.onload = (event) => {
+
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      const notification = new Notification("jdkk");
+    }
+  });
+  const prev = document.getElementById('prev-btn')
+  const next = document.getElementById('next-btn')
+  const list = document.getElementById('item-list')
+
+  const itemWidth = 150
+  const padding = 10
+
+  prev.addEventListener('click', () => {
+    list.scrollLeft -= itemWidth + padding
+  });
+
+  next.addEventListener('click', () => {
+    list.scrollLeft += itemWidth + padding
+  });
+
+
+};
